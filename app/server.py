c87b6a60b9ff40619ecc1742bbf24df40087bc86
Aaron Bassett <me@aaronbassett.com>
2019-02-08 01:34:06 -0500

    Add event webhook endpoint

--- app/server.py
+++ app/server.py
@@ -1,3 +1,4 @@
+import json
 import tornado.ioloop
 import tornado.web
 from tornado import escape
@@ -7,7 +8,7 @@ from logzero import logfile, logger
 logfile("/tmp/pycon-colombia-workshop.log", maxBytes=1e6, backupCount=3)
 
 
-class NCCOHandler(tornado.web.RequestHandler):
+class VAPIServer(tornado.web.RequestHandler):
     def write(self, chunk):
         chunk = escape.json_encode(chunk)
         self.set_header("Content-Type", "application/json; charset=UTF-8")
@@ -15,12 +16,18 @@ class NCCOHandler(tornado.web.RequestHandler):
         self._write_buffer.append(chunk)
 
     def get(self):
-        logger.info("Call received")
+        to = self.get_argument("to", None, True)
+        logger.info(f"NCCO fetched for call to {to}")
         self.write([{"action": "talk", "text": "Hello PyCon"}])
+
+    def post(self):
+        event = json.loads(self.request.body)
+        logger.info(f"Call to {event['to']} status: {event['status']}")
+        self.write([{"status": "ok"}])
 
 
 def make_app():
-    return tornado.web.Application([(r"/ncco", NCCOHandler)])
+    return tornado.web.Application([(r"/", VAPIServer)])
 
 
 if __name__ == "__main__":
